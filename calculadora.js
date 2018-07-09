f0681d438147db066cdd4c5c79b5ec25c8886752
isabellahurtado <isabellahurtado9@gmail.com>
2018-07-08 19:50:06 -0500

    agrgue funciones a la calculadora

--- calculadora.js
+++ calculadora.js
@@ -1,28 +1,38 @@
-var operacion = prompt('1: Volumen de un cilindro 2: Pasar de onzas a toneladas 3: Factorizacion de un numero'); //gragarlo a funciones//
+var operacionDeseada = prompt('1: Volumen de un cilindro 2: Pasar de onzas a toneladas 3: Factorizacion de un numero'); //gragarlo a funciones//
 
-if ( operacion === "1") {
+function volumenCilindro () {
   var radio = prompt ("Ingresa el radio");
   var altura = prompt ("Ingresa la altura");
   var volumenCilindro = (Math.PI * Math.pow (radio,2) * altura);
+  
   alert ("El volumen del cilindro es " + volumenCilindro);
-
   console.log ("El volumen del cilindro es " + volumenCilindro);
 }
 
-if (operacion === "2") {
+function onzasToneladas () {
 	var onzas = prompt ("Ingresa las onzas");
-    var toneladas =  (Math.toneladas *(onzas)); //agregar la formula bien de onzas a toneladas//
+    var toneladas =  (onzas *1 / 35.275 * 1 / 1000); 
+    
     alert ("las toneladas " + toneladas);
-
     console.log ("las toneladas son" + toneladas);
-
 }
  
-if (operacion ==="3") {
+function factorizacion () {
 	var numeroParafactorizar = prompt ("ingresa numero");
-	var resultado = (numeroParafactorizar % resultado == 0);//agregar la formula de factorizacion//
-
+	var resultado = (numeroParafactorizar % resultado == 0);//agregar la formula de factorizacion
+	
 	alert ("Resultado fatorizado  "+ resultado);
 	console.log ("Resultado fatorizado  "+ resultado);
-
 }
+
+ 
+ if (operacionDeseada === "1") {
+ 	volumenCilindro();
+ }
+ else if (operacionDeseada === "2") {
+ 	onzasToneladas();
+ }
+
+ else if  (operacionDeseada === "3") {
+ 	factorizacion();
+ }
